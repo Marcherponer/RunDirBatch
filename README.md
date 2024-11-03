10146d5b7747ab83d10342e736cb9e53447b7e0a
Alpo <alpo.knaap@gmail.com>
2024-11-04 01:00:51 +0200

    updated to main.py, but use command from README

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # RunDirBatch
 Run command for everything in directory
+
+didn't work at all, just use this command:
+for i in ./*; do HandBrakeCLI --preset="Fast 480p30" --all-subtitles --all-audio -i "$i" -o comp/"$i"; done
--- main.py
+++ main.py
@@ -2,16 +2,18 @@ import os
 import subprocess
 
 def main():
-    directory = r"/mnt/media_hdd/Media/anim/You're Under Arrest"
+    directory = "/mnt/media_hdd/Media/anim/You're Under Arrest/"
     presets = ['Fast 480p30', 'Fast 576p25', 'Fast 720p30', 'Fast 1080p30', 'HQ 480p30 Surround', 'HQ 576p25 Surround', 'HQ 720p30 Surround', 'HQ 1080p30 Surround']
-
+    
     for files in sorted(os.listdir(directory)):
-        print('processing ' + files)
-        newName = files.split('.')
-        newName = newName[-2] + '_compressed'
-        newName = ".".join(newName)
-        command = "taskset -c 1,2,3 HandBrakeCLI --preset=\"" + preset[0] + "\" -a \"1,2,3,4,5,6\" --aencoder copy:dtshd -s \"1,2,3,4,5,6\" -i \'" + files + "\' -o '" + newName + "'"
-        subprocess.run()
+        if files.split('.')[-1] == 'mkv':
+            print('processing ' + files)
+            newName = files.split('.')
+            newName[-2] = newName[-2] + '_compressed'
+            newName = ".".join(newName)
+            subprocess.run(["taskset", "-c", "1,2,3", "HandBrakeCLI",  "--preset=\"Fast 480p30\"", "-a", "\"1,2,3,4,5,6\"", "--aencoder", "copy:dtshd", "-s", "\"1,2,3,4,5,6\"", "-i", "\"" + directory + files + "\"", "-o", "\"" + directory + newName + "\""])
+
+    print('done')
     return None
 
 
